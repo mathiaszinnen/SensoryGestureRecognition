f9da68deaae2b64b298ccbc7f1da03c4fb85a9ec
Mathias Zinnen~ <mathias.zinnen@fau.de>
2024-03-28 11:22:21 +0100

    unzip data after downloading

--- tools/download_dataset.py
+++ tools/download_dataset.py
@@ -1,9 +1,15 @@
 from zenodo_get import zenodo_get
+from tqdm import tqdm
+import zipfile
 
 SENSORY_ART_RECORD = '10889613'
 
 def main():
-    zenodo_get([SENSORY_ART_RECORD, '-o data'])
+    zenodo_get([SENSORY_ART_RECORD, '-odata'])
+
+    for f in tqdm(['annotations.zip', 'images.zip']):
+        with zipfile.ZipFile(f'data/{f}') as zf:
+            zf.extractall('data/')
 
 if __name__ == '__main__':
     main()
